41a80f81824ff847d46a4e960341d118807467cd
Cesar Varettoni <cvarettoni@gmail.com>
2022-08-20 13:37:12 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {f07a} from "@fortawesome/free-solid-svg-icons"
 import ItemListContainer from './components/ItemListContainer.js';
 import NavBar from './components/NavBar.js';
 import './components/style.css';
