45dad38869fd9c6d33681bff0dc572ec276fca3b
Cesar Varettoni <cvarettoni@gmail.com>
2022-09-30 00:37:58 -0300

    Detalles finales preentrega

--- src/components/Carrousell.js
+++ src/components/Carrousell.js
@@ -5,7 +5,7 @@ const Carrousell = () => {
     return (
         <section className="fotoHead">
             <h1 className="tituloHead">Para seguir <br/> sintiendo el  <br/> basquetbol en la piel </h1>
-            <Link to="/">
+            <Link to="/list">
                 <button className="botonHead">Comprar</button>
             </Link>
         </section>       
